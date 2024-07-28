46d40891d3654c34330c0a841653b55e56a932ba
markisgod92 <markisgod92@gmail.com>
2024-07-28 19:49:48 +0200

    aggiunta sezione hightlights, display griglia immagini rover, modal per immagini

--- script.js
+++ script.js
@@ -27,26 +27,23 @@ const API_KEY = `LcdKEcVj7T4glrTFvNyVrAQKnVQeFoXgEOEJv5HA`;
 // DOM Elements
 const loadingSpinner = document.getElementById("loading-spinner");
 const loadingError = document.getElementById("loading-failed");
+const roverHightlight = document.getElementById("roverHighlight");
 const sections = document.querySelectorAll("section");
 const neoButtons = document.getElementById("neo-buttons");
 const neoTHead = document.querySelector("#neo-table thead tr")
 const neoTBody = document.querySelector("#neo-table tbody")
 const astroPictureImg = document.getElementById("astro-picture");
 const astroPictureData = document.getElementById("astro-picture-data");
-const marsPictures = document.querySelector("#mars-pictures .carousel-inner");
+const marsPictures = document.getElementById("mars-pictures");
 const roverSelection = document.querySelectorAll("#rover-selection button");
-const marsSwiper = document.getElementById("mars-swiper");
-const swiper = new Swiper(`.swiper`, {
-    direction: "horizontal",
-    navigation: {
-        prevEl: ".swiper-button-prev",
-        nextEl: ".swiper-button-next"
-    },
-    centeredSlides: `auto`,
-    effect: "coverflow"
-});
+const loadMoreBtn = document.getElementById("loadMoreRover");
 const svg = d3.select("#asteroidMap")
 const tooltip = d3.select("#tooltip");
+const formatter = new Intl.NumberFormat("it-IT", {
+    style: "decimal",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+})
 
 
 const getTodayDate = () => {
@@ -79,8 +76,40 @@ const getNeowsData = async () => {
 
 const orderData = (data) => {
     const order = Object.keys(data).sort((a, b) => new Date(a) - new Date(b));
+    createNeoHighlight(order, data)
+    createDateButtons(order, data);
+}
 
-    order.forEach((element, index) => {
+const getSingleNeoData = (data) => {
+    return object = {
+        name: data.name,
+        magnitude: data.absolute_magnitude_h,
+        closeApproach: data.close_approach_data[0].close_approach_date_full,
+        distance: data.close_approach_data[0].miss_distance.kilometers,
+        speed: data.close_approach_data[0].relative_velocity.kilometers_per_second,
+        minDiameter: data.estimated_diameter.meters.estimated_diameter_min,
+        maxDiameter: data.estimated_diameter.meters.estimated_diameter_max,
+        hazard: data.is_potentially_hazardous_asteroid ? "Yes" : "No"
+    }
+}
+
+const createNeoHighlight = (orderedData, data) => {
+    const randomDayData = data[orderedData[Math.floor(Math.random() * orderedData.length)]]
+    const object = getSingleNeoData(randomDayData[Math.floor(Math.random() * randomDayData.length)])
+
+    document.querySelector("#neoHighlight h5").innerText = object.name;
+
+    document.querySelector("#neoHighlight p:first-of-type").innerText = `Close Approach Date: ${object.closeApproach}`;
+
+    document.querySelector("#neoHighlight p:nth-of-type(2)").innerText = `Distance: ${formatter.format(object.distance)} km`;
+
+    document.querySelector("#neoHighlight p:nth-of-type(3)").innerText = `Speed: ${formatter.format(object.speed)} km/s`;
+
+    document.querySelector("#neoHighlight p:last-of-type").innerText = `Diameter: ${formatter.format(object.minDiameter)} m - ${formatter.format(object.maxDiameter)} m`;
+}
+
+const createDateButtons = (orderedData, data) => {
+    orderedData.forEach((element, index) => {
         const button = document.createElement("button");
         button.setAttribute("class", "btn btn-secondary-outline neows-btn");
         button.innerText = element;
@@ -97,17 +126,10 @@ const orderData = (data) => {
 const drawNeo = (data) => {
     svg.selectAll("*").remove();
 
-    const asteroidData = Object.values(data).flat().map(asteroid => {
-        return {
-            name: asteroid.name,
-            size: asteroid.estimated_diameter.kilometers.estimated_diameter_max,
-            missDistance: asteroid.close_approach_data[0].miss_distance.kilometers,
-            orbitingBody: asteroid.close_approach_data[0].orbiting_body
-        };
-    });
+    const asteroidData = Object.values(data).flat().map(getSingleNeoData);
 
     const sizeScale = d3.scaleSqrt()
-        .domain([0, d3.max(asteroidData, d => d.size)])
+        .domain([0, d3.max(asteroidData, d => d.maxDiameter)])
         .range([2, 20]);
 
     const angleScale = d3.scaleLinear()
@@ -115,22 +137,22 @@ const drawNeo = (data) => {
         .range([0, 2 * Math.PI]);
 
     const distanceScale = d3.scaleLog()
-        .domain([d3.min(asteroidData, d => d.missDistance), d3.max(asteroidData, d => d.missDistance)])
+        .domain([d3.min(asteroidData, d => d.distance), d3.max(asteroidData, d => d.distance)])
         .range([50, 400]);
 
     svg.selectAll("circle")
         .data(asteroidData)
         .enter()
         .append("circle")
-        .attr("cx", d => distanceScale(d.missDistance) * Math.cos(angleScale(asteroidData.indexOf(d))))
-        .attr("cy", d => distanceScale(d.missDistance) * Math.sin(angleScale(asteroidData.indexOf(d))))
-        .attr("r", d => sizeScale(d.size))
+        .attr("cx", d => distanceScale(d.distance) * Math.cos(angleScale(asteroidData.indexOf(d))))
+        .attr("cy", d => distanceScale(d.distance) * Math.sin(angleScale(asteroidData.indexOf(d))))
+        .attr("r", d => sizeScale(d.maxDiameter))
         .attr("fill", "orange")
         .attr("stroke", "white")
         .attr("stroke-width", 1)
         .on("mouseover", (event, d) => {
             tooltip.style("opacity", 1)
-                .html(`Name: ${d.name}<br>Size: ${d.size.toFixed(2)} km<br>Miss Distance: ${d.missDistance} km`)
+                .html(`Name: ${d.name}<br>Size: ${formatter.format(d.maxDiameter)} km<br>Miss Distance: ${formatter.format(d.distance)} km`)
                 .style("left", `${event.pageX + 10}px`)
                 .style("top", `${event.pageY - 20}px`);
         })
@@ -167,43 +189,34 @@ const displayNeowsData = (currentBtn, key, data) => {
         const tr = document.createElement("tr");
         tr.setAttribute("scope", "row")
 
-        createNeoTD(item, tr)
+        const objectData = getSingleNeoData(item)
+        createNeoTD(objectData, tr)
 
         neoTBody.appendChild(tr)
     })
 }
 
 const createNeoTD = (item, container) => {
-    const formatter = new Intl.NumberFormat("it-IT", {
-        style: "decimal",
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2
-    })
-
     const name = document.createElement("td");
     name.innerText = item.name;
 
     const magnitude = document.createElement("td");
-    magnitude.innerText = item.absolute_magnitude_h;
+    magnitude.innerText = item.magnitude;
 
     const closeApproach = document.createElement("td");
-    closeApproach.innerText = item.close_approach_data[0].close_approach_date_full;
+    closeApproach.innerText = item.closeApproach;
 
     const distance = document.createElement("td");
-    distance.innerText = `${formatter.format(item.close_approach_data[0].miss_distance.kilometers)} km`;
+    distance.innerText = `${formatter.format(item.distance)} km`;
 
     const speed = document.createElement("td");
-    speed.innerText = `${formatter.format(item.close_approach_data[0].relative_velocity.kilometers_per_second)} km/s`;
+    speed.innerText = `${formatter.format(item.speed)} km/s`;
 
     const diameter = document.createElement("td");
-    diameter.innerText = `${formatter.format(item.estimated_diameter.meters.estimated_diameter_min)} m - ${formatter.format(item.estimated_diameter.meters.estimated_diameter_max)} m`;
+    diameter.innerText = `${formatter.format(item.minDiameter)} m - ${formatter.format(item.maxDiameter)} m`;
 
     const hazard = document.createElement("td");
-    if (item.is_potentially_hazardous_asteroid) {
-        hazard.innerText = "Yes";
-    } else {
-        hazard.innerText = "No";
-    }
+    hazard.innerText = item.hazard;
 
     container.append(name, magnitude, closeApproach, distance, speed, diameter, hazard);
 }
@@ -211,20 +224,36 @@ const createNeoTD = (item, container) => {
 
 
 // Image of the Day
-const imgUrl = `https://api.nasa.gov/planetary/apod`;
+const imgUrl = `https://api.nasa.gov/planetary/apod?thumbs=true`;
 
 const getImg = async () => {
     try {
-        const response = await fetch(`${imgUrl}?api_key=${API_KEY}`);
+        const response = await fetch(`${imgUrl}&api_key=${API_KEY}`);
         const data = await response.json();
+        displayApodHighlight(data);
         displayImg(data);
     } catch (error) {
         console.log("img error", error)
     }
 }
 
+const displayApodHighlight = (data) => {
+    if (data.media_type === "image") {
+        document.querySelector("#apodHighlight img").src = data.hdurl;
+    } else if (data.media_type === "video") {
+        document.querySelector("#apodHighlight img").src = data.thumbnail_url;
+    }
+    document.querySelector("#apodHighlight img").alt = data.title;
+    
+    document.querySelector("#apodHighlight p").innerText = data.title;
+}
+
 const displayImg = (data) => {
-    astroPictureImg.src = data.hdurl;
+    if (data.media_type === "image") {
+        astroPictureImg.src = data.hdurl;
+    } else if (data.media_type === "video") {
+        astroPictureImg.src = data.thumbnail_url;
+    }
     astroPictureImg.alt = data.title;
 
     const title = document.createElement("h5");
@@ -238,63 +267,125 @@ const displayImg = (data) => {
     description.innerText = data.explanation;
 
     astroPictureData.append(title, copyright, description)
+
+    astroPictureImg.addEventListener("click", () => displayApodModal(data))
 }
 
+const displayApodModal = (data) => {
+    const modal = new bootstrap.Modal(document.getElementById('picModal'));
+
+    if (data.media_type === "image") {
+        document.querySelector("#picModal img").src = data.hdurl;
+    } else if (data.media_type === "video") {
+        document.querySelector("#picModal img").src = data.thumbnail_url;
+    }
+    document.querySelector("#picModal img").alt = data.title;
+
+    const title = document.createElement("p");
+    title.innerText = data.title;
+
+    const copyright = document.createElement("p");
+    copyright.innerText = data.copyright;
+
+    document.querySelector("#picModal .modal-footer").replaceChildren();
+    document.querySelector("#picModal .modal-footer").append(title, copyright);
+
+    modal.show();
+}
 
 
 // Mars Rover
 const marsUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/`
+const itemsPerPage = 12;
+let currentPage = 0;
+let currentRoverData = [];
 
 const getMarsData = async (rover = "perseverance") => {
-    let today = getTodayDate();
-
     try {
         const response = await fetch(`${marsUrl}/${rover}/latest_photos?api_key=${API_KEY}`);
         const data = await response.json();
-        displayMarsImg(data.latest_photos);
+        displayRoverHighlight(data.latest_photos);
+        currentRoverData = data.latest_photos;
+        currentPage = 0;
+        displayMarsImg();
     } catch (error) {
         console.log("rover data error", error);
     }
 }
 
-const displayMarsImg = (data) => {
-    data.forEach((item, index) => {
-        const swiperSlide = document.createElement("div");
-        swiperSlide.setAttribute("class", "swiper-slide d-flex flex-column justify-content-center align-items-center gap-3");
+const displayRoverHighlight = (data) => {
+    const randomImg = data[Math.floor(Math.random() * data.length)]
+    roverHightlight.src = randomImg.img_src;
+    roverHightlight.alt = randomImg.id;
+}
 
-        const imgWrapper = document.createElement("div");
-        imgWrapper.setAttribute("class", "w-50");
+const displayMarsImg = () => {
+    const start = currentPage * itemsPerPage;
+    const end = start + itemsPerPage;
+    const toLoad = currentRoverData.slice(start, end);
+    console.log(toLoad)
+
+    toLoad.forEach(item => {
+        const col = document.createElement("div");
+        col.setAttribute("class", "col");
+
+        const card = document.createElement("div");
+        card.setAttribute("class", "card h-100 mars-card border-0");
 
         const img = document.createElement("img");
-        img.setAttribute("class", "w-100 object-fit-cover");
+        img.setAttribute("class", "h-100 object-fit-cover");
         img.src = item.img_src;
         img.alt = item.id;
 
-        const caption = document.createElement("div");
-        caption.setAttribute("class", "d-flex justify-content-center gap-3 text-light")
+        card.appendChild(img);
+        col.appendChild(card);
+        marsPictures.appendChild(col);
 
-        const sol = document.createElement("p");
-        sol.innerText = `Sol: ${item.sol}`;
-
-        const date = document.createElement("p");
-        date.innerText = `Earth date: ${item.earth_date}`;
-
-        const camera = document.createElement("p");
-        camera.innerText = `Camera: ${item.camera.name} - ${item.camera.full_name}`;
-
-        imgWrapper.appendChild(img);
-        caption.append(sol, date, camera);
-        swiperSlide.append(imgWrapper, caption);
-        marsSwiper.appendChild(swiperSlide);
+        card.addEventListener("click", () => displayMarsModal(item))
     })
+
+    currentPage++;
+
+    if (currentPage * itemsPerPage < currentRoverData.length) {
+        loadMoreBtn.classList.add("d-block")
+        loadMoreBtn.classList.remove("d-none")
+    } else {
+        loadMoreBtn.classList.remove("d-block")
+        loadMoreBtn.classList.add("d-none")
+    }
 }
+
+loadMoreBtn.addEventListener("click", displayMarsImg)
 
 roverSelection.forEach(button => {
     button.addEventListener("click", () => {
-        marsSwiper.replaceChildren();
+        roverSelection.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
+        marsPictures.replaceChildren();
         getMarsData(button.value);
     })
 })
+
+const displayMarsModal = (data) => {
+    const modal = new bootstrap.Modal(document.getElementById('picModal'));
+
+    document.querySelector("#picModal img").src = data.img_src;
+    document.querySelector("#picModal img").alt = data.id;
+
+    const sol = document.createElement("p");
+    sol.innerText = `Sol: ${data.sol}`;
+
+    const date = document.createElement("p");
+    date.innerText = `Earth date: ${data.earth_date}`;
+
+    const camera = document.createElement("p");
+    camera.innerText = `Camera: ${data.camera.name} - ${data.camera.full_name}`;
+
+    document.querySelector("#picModal .modal-footer").replaceChildren();
+    document.querySelector("#picModal .modal-footer").append(sol, date, camera);
+
+    modal.show();
+}
 
 
 // LOADING
